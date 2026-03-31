dfbc55521412dfa760a6fc54509b66ec027298b2
rafael <rafael.santos7520@gmail.com>
2026-03-31 09:05:23 -0300

    chore: relax typescript and eslint strictness rules for unused variables and type assertions

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,13 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'no-unused-vars': 'off',
+      'react-refresh/only-export-components': 'warn',
+    },
   },
 ])
