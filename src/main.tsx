4012801a6673e92c257db35990d58777797a4ecf
Rafael Santos <rafael.santos7520@gmail.com>
2026-04-20 18:35:58 -0300

    ajuste query bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {QueryClientProvider} from '@tanstack/react-query'
 import {AuthProvider} from './contexts/AuthContext'
+import {queryClient} from './lib/queryClient'
 import './index.css'
 import App from './App'
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 30 * 1000,
-            retry: 1,
-            refetchOnWindowFocus: false,
-        },
-    },
-})
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
